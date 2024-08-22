2eead4d5949ac174bee393eb44f6fe29d2a600a4
yuya2102219 <yuya25142203@gmail.com>
2024-08-22 14:52:23 +0900

    4日目途中提出（再）

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
 Deno.serve(async (req) => {
   const pathname = new URL(req.url).pathname;
+  console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
     return new Response("jigインターンへようこそ！");
@@ -10,7 +11,7 @@ Deno.serve(async (req) => {
   return serveDir(req, {
     fsRoot: "public",
     urlRoot: "",
-    showDirListing: false, 
+    showDirListing: true,
     enableCors: true,
   });
 });
